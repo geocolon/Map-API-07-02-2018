6f65e830ea53fb9688d6f9fb25f4736b12c16a2b
George Colon <geocolon@gmail.com>
2018-08-13 12:12:48 -0700

    removed test folder

--- client/src/test/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MyFancyComponent from './smartComponents/ChatComp';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MyFancyComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
